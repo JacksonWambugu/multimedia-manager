9cee803fb1e3c90480f98f81825441e7a1c8a81e
JacksonWambugu <jaksehsekhja@gmail.com>
2023-06-27 02:29:53 +0300

    commit 1

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    name: "Video File",
+    type: "video",
+    path: "/file-server/vid.mp4",
+  },
+  {
+    id: 2,
+    name: "Audio File",
+    type: "audio",
+    path: "/file-server/mp.mp3",
+  },
+  {
+    id: 3,
+    name: "Document File",
+    type: "document",
+    path: "/file-server/pdf.pdf",
+  },
+  {
+    id: 4,
+    name: "Image File",
+    type: "image",
+    path: "/file-server/image.png",
+  },
+];
